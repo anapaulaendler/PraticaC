9c246747a3ed6f1335a02a93a08a4743b619a59e
ana ♡ <141460950+mirtilos@users.noreply.github.com>
2023-11-22 07:23:49 -0300

    Update structAltura.c

--- structAltura.c
+++ structAltura.c
@@ -3,40 +3,83 @@
 
 typedef struct {
     char nome[50];
-    int altura;
-} pessoa;
+    float altura;
+} Pessoa;
 
 int main() {
+    Pessoa pessoas[10];
+    int opcao;
+    float media = 0;
 
-    pessoa p[10];
-    int media = 0;
+    do {
+        printf("\nmenu:\n");
+        printf("1. cadastrar os 10 registros\n");
+        printf("2. apresentar os registros das pessoas com 1.50m ou menores\n");
+        printf("3. apresentar os registros das pessoas com mais de 1.50m\n");
+        printf("4. apresentar os registros das pessoas com mais de 1.5m e menos de 1.80m\n");
+        printf("5. calcular a média das alturas e apresentar os registros das pessoas acima da média\n");
+        printf("0. sair\n");
+        printf("escolha uma opção: ");
+        scanf("%d", &opcao);
 
-    for (int i = 0; i < 10; i++) {
-        
-        fflush(stdin);
-        int lixo = getchar();
-        scanf("%[^\n]s", p[i].nome);
-        scanf("%d", &p[i].altura);
-        media += p[i].altura;
-    }
+        switch (opcao) {
+            case 1:
+                for (int i = 0; i < 10; i++) {
+                    printf("digite o nome da pessoa %d: ", i + 1);
+                    scanf("%s", pessoas[i].nome);
 
-    for (int i = 0; i < 10; i++) {
-        if (p[i].altura <= 150) {
-            printf("%s, %d cm\n", p[i].nome, p[i].altura);
-        } else if (p[i].altura > 150 && p[i].altura <= 180) {
-            printf("%s, %d cm\n", p[i].nome, p[i].altura);
-        } else if (p[i].altura > 180) {
-            printf("%s, %d cm\n", p[i].nome, p[i].altura);
+                    printf("digite a altura da pessoa %d (em metros): ", i + 1);
+                    scanf("%f", &pessoas[i].altura);
+
+                    media += pessoas[i].altura;
+                }
+                media /= 10;
+                break;
+
+            case 2:
+                printf("\nregistros das pessoas com 1.50m ou menores:\n");
+                for (int i = 0; i < 10; i++) {
+                    if (pessoas[i].altura <= 1.50) {
+                        printf("%s, %.2fm\n", pessoas[i].nome, pessoas[i].altura);
+                    }
+                }
+                break;
+
+            case 3:
+                printf("\nregistros das pessoas com mais de 1.50m:\n");
+                for (int i = 0; i < 10; i++) {
+                    if (pessoas[i].altura > 1.50) {
+                        printf("%s, %.2fm\n", pessoas[i].nome, pessoas[i].altura);
+                    }
+                }
+                break;
+
+            case 4:
+                printf("\nregistros das pessoas com mais de 1.50m e menos de 1.80m:\n");
+                for (int i = 0; i < 10; i++) {
+                    if (pessoas[i].altura > 1.50 && pessoas[i].altura < 1.80) {
+                        printf("%s, %.2fm\n", pessoas[i].nome, pessoas[i].altura);
+                    }
+                }
+                break;
+
+            case 5:
+                printf("\nregistros das pessoas acima da média de altura (%.2fm):\n", media);
+                for (int i = 0; i < 10; i++) {
+                    if (pessoas[i].altura > media) {
+                        printf("%s, %.2fm\n", pessoas[i].nome, pessoas[i].altura);
+                    }
+                }
+                break;
+
+            case 0:
+                printf("Saindo do programa.\n");
+                break;
+
+            default:
+                printf("Opção inválida. Tente novamente.\n");
         }
-    }
-
-    printf("media: %d cm\n", media / 10);
-
-    for (int i = 0; i < 10; i++) {
-        if (p[i].altura > media) {
-            printf("%s, %d cm\n", p[i].nome, p[i].altura);
-        }
-    }
+    } while (opcao != 0);
 
     return 0;
 }
